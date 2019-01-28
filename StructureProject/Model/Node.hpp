7c63eeb18448e7fac5bacafbc2de46b9b216de71
rphi8796 <jlkfop@gmail.com>
2019-01-28 09:25:29 -0700

    added to node class

---
+++ StructureProject/Model/Node.hpp
@@ -0,0 +1,46 @@
+//
+//  Header.h
+//  StructureProject
+//
+//  Created by Phillips, Ryan on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+#include <assert.h>
+
+template <class Type>
+class Node
+{
+private:
+    Type data;
+public:
+    Node(Type data);
+    Type getData();
+    void setData(Type data);
+};
+
+
+//Template Definitions
+
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this->data = data;
+}
+
+template <class Type>
+Type Node<Type> :: getData()
+{
+    return data;
+}
+
+template <class Type>
+void Node<Type> :: setData(Type data)
+{
+    this->data = data;
+}
+
+#endif /* Node_hpp */
