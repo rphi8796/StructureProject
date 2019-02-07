c4adc7c3d67d45a43d3b66473c6bd59ce6f4c3ad
rphi8796 <jlkfop@gmail.com>
2019-02-07 09:45:19 -0700

    I added the constructors, destructors, and one helper method

--- StructureProject/Model/Linear/LinkedList.hpp
+++ StructureProject/Model/Linear/LinkedList.hpp
@@ -37,9 +37,49 @@ public:
     Type remove(int index);
 //    Type setAtIndex(int index, Type item);
 //    bool contains(Type item);
-    
 };
 
+//Constructor
+template <class Type>
+LinkedList<Type> :: linkedList()
+{
+    this->front = nullptr;
+    this->end = nullptr;
+    this->size = 0;
+}
+
+//Destructor
+template <class Type>
+LinkedList<Type> :: ~LinkedList()
+{
+    LinearNode<Type> * destroyStructure = front;
+    while (front != nullptr)
+    {
+        front = destroyStructure->getNextNode();
+        delete destroyStructure;
+        destroyStructure = front;
+    }
+}
+
+//Helper methods
+template <class Type>
+void LinkedList<Type> :: add(Type item)
+{
+    LinearNode<Type> * newData = new LinearNode<Type>(item);
+    
+    if(this->size= 0)
+    {
+        this->front = newData;
+    }
+    else
+    {
+        this->end->setNextNode(newData);
+    }
+    
+    this->end = newData;
+    
+    this->size += 1;
+}
 
 
 #endif /* LinkedList_hpp */
